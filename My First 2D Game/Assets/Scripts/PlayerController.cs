ba174158c3aa62b2e501e543b62b0fa2f0ca9cef
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-08 21:59:10 +0800

    实现使用键盘控制人物简单移动

--- My First 2D Game/Assets/Scripts/PlayerController.cs
+++ My First 2D Game/Assets/Scripts/PlayerController.cs
@@ -13,12 +13,21 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.right * speed * Time.deltaTime);
+        float moveX = Input.GetAxisRaw("Horizontal");   // 控制水平方向移动 A:-1  D: 1
+        float moveY = Input.GetAxisRaw("Vertical");     // 控制垂直方向移动 W:-1  S: 1
+
+        Vector2 position = transform.position;          // 人物当前位置
+        
+        // 更新人物位置
+        position.x += moveX * speed * Time.deltaTime;   
+        position.y += moveY * speed * Time.deltaTime;
+
+        transform.position = position;
     }
 }
