a394eed92e59e2f704603f6d4defaa7072486bae
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-09 10:40:59 +0800

    实现伤害陷阱

---
+++ My First 2D Game/Assets/Scripts/DamageArea.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// …À∫¶œ›⁄Âœ‡πÿ
+/// </summary>
+public class DamageArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ≈ˆ◊≤ºÏ≤‚œ‡πÿ
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerStay2D(Collider2D otherCollider)
+    {
+        PlayerController playerController = otherCollider.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            if (playerController.MyCurrentHealth > 0)
+            {
+                playerController.ChangeHealth(-1);
+                // Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- My First 2D Game/Assets/Scripts/PlayerController.cs
+++ My First 2D Game/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;    // 移动速度
+    public float speed = 5f;            // 移动速度
 
-    private int maxHealth = 5;  // 最大生命值
+    private int maxHealth = 5;          // 最大生命值
 
-    private int currentHealth = 1;  // 当前生命值
+    private int currentHealth;      // 当前生命值
 
-    Rigidbody2D rigidBody;      // 刚体组件
+    private float invincibleTime = 2f;  // 无敌时间
+
+    private float invincibleTimer;      // 无敌计时器
+
+    private bool isInvincible;          // 是否处于无敌
+
+    Rigidbody2D rigidBody;              // 刚体组件
+
+    public int MyMaxHealth { get { return maxHealth; } }
+    public int MyCurrentHealth { get { return currentHealth; } }
 
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = 2;
+        invincibleTimer = 0;
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -28,20 +39,27 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");   // 控制水平方向移动 A:-1  D: 1
         float moveY = Input.GetAxisRaw("Vertical");     // 控制垂直方向移动 W:-1  S: 1
 
-        // 人物移动
-        // 通过移动刚体位置而非人物位置解决人物与其他物体碰撞时抖动问题
+        // 人物移动: 通过移动刚体位置而非人物位置解决人物与其他物体碰撞时抖动问题
         //=========================================================================================================
         Vector2 position = rigidBody.position;          // 人物当前位置
-        
+
         // 更新人物位置
-        position.x += moveX * speed * Time.deltaTime;   
+        position.x += moveX * speed * Time.deltaTime;
         position.y += moveY * speed * Time.deltaTime;
 
         rigidBody.position = position;
-    }
 
-    public int MyMaxHealth { get { return maxHealth; } }
-    public int MyCurrentHealth { get { return currentHealth; } }
+        // 无敌计时
+        //=========================================================================================================
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
 
     /// <summary>
     /// 改变玩家生命值
@@ -49,6 +67,17 @@ public class PlayerController : MonoBehaviour
     /// <param name="amount"></param>
     public void ChangeHealth(int amount)
     {
+        // 如果人物受到伤害
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = invincibleTime;
+        }
+
         // 把玩家生命值约束在 0 和 最大值 之间
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
