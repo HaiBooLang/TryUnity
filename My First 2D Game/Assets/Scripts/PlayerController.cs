a472661f3db01b838d3bb3fd367673e949a26cab
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-09 14:03:43 +0800

    实现人物动画

--- My First 2D Game/Assets/Scripts/PlayerController.cs
+++ My First 2D Game/Assets/Scripts/PlayerController.cs
@@ -22,8 +22,13 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rigidBody;              // 刚体组件
 
+    Animator animator;
+
     public int MyMaxHealth { get { return maxHealth; } }
     public int MyCurrentHealth { get { return currentHealth; } }
+
+    // 玩家方向
+    private Vector2 lookDirection = new Vector2 (1, 0);     // 默认看向右方
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +36,7 @@ public class PlayerController : MonoBehaviour
         currentHealth = 2;
         invincibleTimer = 0;
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,15 +45,23 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");   // 控制水平方向移动 A:-1  D: 1
         float moveY = Input.GetAxisRaw("Vertical");     // 控制垂直方向移动 W:-1  S: 1
 
+        Vector2 moveVector = new Vector2 (moveX, moveY);
+        if(moveVector.x!=0 || moveVector.y != 0)
+        {
+            lookDirection = moveVector;
+        }
+
+        animator.SetFloat("Look X",lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", moveVector.magnitude);
+
         // 人物移动: 通过移动刚体位置而非人物位置解决人物与其他物体碰撞时抖动问题
         //=========================================================================================================
         Vector2 position = rigidBody.position;          // 人物当前位置
 
-        // 更新人物位置
-        position.x += moveX * speed * Time.deltaTime;
-        position.y += moveY * speed * Time.deltaTime;
+        position += moveVector * speed * Time.deltaTime;
 
-        rigidBody.position = position;
+        rigidBody.MovePosition(position);
 
         // 无敌计时
         //=========================================================================================================
