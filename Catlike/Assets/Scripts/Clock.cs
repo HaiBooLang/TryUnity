2c037a8fa288ca38099980b62701732fd49e8f6d
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-22 23:47:43 +0800

    Knowing all Three Arms

--- Catlike/Assets/Scripts/Clock.cs
+++ Catlike/Assets/Scripts/Clock.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-public class Clock : MonoBehaviour {
-    
+public class Clock : MonoBehaviour
+{
     [SerializeField]
-    Transform   hoursPivot;
+    Transform hoursPivot, minutesPivot, secondsPivot;
 }
