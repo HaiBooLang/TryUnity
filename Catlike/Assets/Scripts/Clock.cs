c159d99a6f8e253e28b431439f7739b1ab2ab42d
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-22 23:45:02 +0800

    Getting Hold of an Arm

--- Catlike/Assets/Scripts/Clock.cs
+++ Catlike/Assets/Scripts/Clock.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Clock : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Clock : MonoBehaviour {
+    
+    [SerializeField]
+    Transform   hoursPivot;
 }
