98cb31cd7ef327bd86ec4d6392ee3c0af10f29ad
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 00:04:43 +0800

    Rotating the Arms

--- Catlike/Assets/Scripts/Clock.cs
+++ Catlike/Assets/Scripts/Clock.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
 
     private void Awake()
     {
-        Debug.Log(DateTime.Now);
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+        DateTime time = DateTime.Now;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
     }
 }
