7d331a34cd115921f6df049c80b4792013a1aeef
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 09:36:23 +0800

    Using X to Define Y

--- Catlike/Assets/Scripts/Graph.cs
+++ Catlike/Assets/Scripts/Graph.cs
@@ -15,6 +15,7 @@ public class Graph : MonoBehaviour
         {
             Transform point = Instantiate(pointPrefab);
             position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
         }
