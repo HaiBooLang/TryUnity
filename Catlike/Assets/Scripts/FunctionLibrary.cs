a4441aa41e2891436ddeb59f41abbfad23466890
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 12:09:51 +0800

    Implicitly using a Type

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 
+using static UnityEngine.Mathf;
+
 public static class FunctionLibrary
 {
 
     public static float Wave(float x, float t)
     {
-        return Mathf.Sin(Mathf.PI * (x + t));
+        return Sin(PI * (x + t));
+    }
+
+    public static float MultiWave(float x, float t)
+    {
+        float y = Sin(PI * (x + t));
+        y += 0.5f * Sin(2f * PI * (x + t));
+        return y * (2f / 3f);
     }
 }
---
+++ Catlike/Assets/Scripts/NewGraph.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewGraph : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform pointPrefab;
+    
+    [SerializeField, Range(10,100)]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+        
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = FunctionLibrary.MultiWave(position.x, time);
+            point.localPosition = position;
+        }
+    }
+}
