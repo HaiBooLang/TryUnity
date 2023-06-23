7e4ad4ef056042394d4c06a488e30a98bbb08912
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 11:49:39 +0800

    Function Method

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -1,3 +1,10 @@
 using UnityEngine;
 
-public static class FunctionLibrary { }
+public static class FunctionLibrary
+{
+
+    public static float Wave(float x, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+}
