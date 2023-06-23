08d04af01e48a67113ba8c8cf8d7f86944adb3cb
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 09:50:55 +0800

    Variable Instantiation

--- Catlike/Assets/Scripts/Graph.cs
+++ Catlike/Assets/Scripts/Graph.cs
@@ -12,15 +12,18 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
+        float step = 2f / resolution;
         Vector3 position = Vector3.zero;
-        var scale = Vector3.one / 5f;
-        for (int i = 0; i < 10; i++)
+        var scale = Vector3.one * step;
+
+        for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
+            position.x = (i + 0.5f) * step - 1f;
             position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
         }
     }
 }
