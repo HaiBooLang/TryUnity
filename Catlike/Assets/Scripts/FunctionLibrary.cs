1d80f94dcb1a81ba4f28f64c0c06885ec579dc35
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 13:39:15 +0800

    Incorporating Z

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -18,19 +18,20 @@ public static class FunctionLibrary
 
     public static float Wave(float x, float z, float t)
     {
-        return Sin(PI * (x + t));
+        return Sin(PI * (x + z + t));
     }
 
     public static float MultiWave(float x, float z, float t)
     {
         float y = Sin(PI * (x + t));
-        y += 0.5f * Sin(2f * PI * (x + t));
-        return y * (2f / 3f);
+        y += 0.5f * Sin(2f * PI * ((z + t) + t));
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f);
     }
 
     public static float Ripple(float x, float z, float t)
     {
-        float d = Abs(x);
+        float d = Sqrt(x * x + z * z);
         float y = Sin(PI * (4f * d - t));
         return y / (1f + 10f * d);
     }
