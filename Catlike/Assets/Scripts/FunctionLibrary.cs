ba1b1911d085d86e4e439b2d5114b90b91dd1561
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 15:08:41 +0800

    Creating a Torus (1)

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 using static UnityEngine.Mathf;
@@ -7,9 +8,9 @@ public static class FunctionLibrary
 
     public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName { Wave, MultiWave, Ripple, Sphere }
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
 
-    static Function[] functions = { Wave, MultiWave, Ripple, Sphere };
+    static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
     public static Function GetFunction(FunctionName name)
     {
@@ -52,6 +53,17 @@ public static class FunctionLibrary
     {
         float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
         float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
+
+    public static Vector3 Torus(float u, float v, float t)
+    {
+        float r = 1f;
+        float s = 0.5f + r * Cos(0.5f * PI * v);
         Vector3 p;
         p.x = s * Sin(PI * u);
         p.y = r * Sin(0.5f * PI * v);
