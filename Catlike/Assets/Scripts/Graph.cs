5d3004c627d8a44ece26d73bea59ead72abab870
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 11:23:07 +0800

    Updating the Points

--- Catlike/Assets/Scripts/Graph.cs
+++ Catlike/Assets/Scripts/Graph.cs
@@ -23,10 +23,19 @@ public class Graph : MonoBehaviour
         {
             Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = position.x * position.x * position.x;
+            point.localPosition = position;
         }
     }
 }
