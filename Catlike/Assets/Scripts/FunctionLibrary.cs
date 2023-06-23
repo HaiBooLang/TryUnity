66986333d7ad66145472aaa4a82c0de6ddbf9d38
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 13:59:06 +0800

    Three-Dimensional Functions

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -5,7 +5,7 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {
 
-    public delegate float Function(float x, float z, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
     public enum FunctionName { Wave, MultiWave, Ripple }
 
@@ -16,23 +16,35 @@ public static class FunctionLibrary
         return functions[(int)name];
     }
 
-    public static float Wave(float x, float z, float t)
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + z + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    public static float MultiWave(float x, float z, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + t));
-        y += 0.5f * Sin(2f * PI * ((z + t) + t));
-        y += Sin(PI * (x + z + 0.25f * t));
-        return y * (1f / 2.5f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        return p;
     }
 
-    public static float Ripple(float x, float z, float t)
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Sqrt(x * x + z * z);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
     }
 }
--- Catlike/Assets/Scripts/NewGraph.cs
+++ Catlike/Assets/Scripts/NewGraph.cs
@@ -22,17 +22,9 @@ public class NewGraph : MonoBehaviour
         var scale = Vector3.one * step;
 
         points = new Transform[resolution * resolution];
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        for (int i = 0; i < points.Length; i++)
         {
-            if (x == resolution)
-            {
-                x = 0;
-                z += 1;
-            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
@@ -42,12 +34,18 @@ public class NewGraph : MonoBehaviour
     {
         float time = Time.time;
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-        for (int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
