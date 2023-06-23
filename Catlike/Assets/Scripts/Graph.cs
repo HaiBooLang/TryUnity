02d151296ed4ce59051531442702f4cd4afc1113
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 09:23:51 +0800

    Instantiating Prefabs

--- Catlike/Assets/Scripts/Graph.cs
+++ Catlike/Assets/Scripts/Graph.cs
@@ -5,4 +5,13 @@ public class Graph : MonoBehaviour
 
     [SerializeField]
     Transform pointPrefab;
+
+    void Awake()
+    {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+
+        point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2f;
+    }
 }
