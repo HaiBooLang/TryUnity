7ba10c3c5858bb4d04ba5dd2be6e00b69ed07bf1
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 16:49:43 +0800

    Frame Durations

--- Catlike/Assets/Scripts/FrameRateCounter.cs
+++ Catlike/Assets/Scripts/FrameRateCounter.cs
@@ -7,6 +7,11 @@ public class FrameRateCounter : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI display;
 
+    public enum DisplayMode { FPS, MS }
+
+    [SerializeField]
+    DisplayMode displayMode = DisplayMode.FPS;
+
     [SerializeField, Range(0.1f, 2f)]
     float sampleDuration = 1f;
 
@@ -31,12 +36,24 @@ public class FrameRateCounter : MonoBehaviour
 
         if (duration >= sampleDuration)
         {
-            display.SetText(
-                "FPS\n{0:0}\n{1:0}\n{2:0}",
-                1f / bestDuration,
-                frames / duration,
-                1f / worstDuration
-            );
+            if (displayMode == DisplayMode.FPS)
+            {
+                display.SetText(
+                    "FPS\n{0:0}\n{1:0}\n{2:0}",
+                    1f / bestDuration,
+                    frames / duration,
+                    1f / worstDuration
+                );
+            }
+            else
+            {
+                display.SetText(
+                    "MS\n{0:1}\n{1:1}\n{2:1}",
+                    1000f * bestDuration,
+                    1000f * duration / frames,
+                    1000f * worstDuration
+                );
+            }
             frames = 0;
             duration = 0f;
             bestDuration = float.MaxValue;
