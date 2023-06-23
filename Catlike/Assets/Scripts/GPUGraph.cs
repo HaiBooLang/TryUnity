593dcb49dd02ccb7aa376fad8132d73d9c2f1f1b
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 23:04:51 +0800

    Dispatching a Compute Shader Kernel

--- Catlike/Assets/Scripts/GPUGraph.cs
+++ Catlike/Assets/Scripts/GPUGraph.cs
@@ -17,6 +17,26 @@ public class GPUGraph : MonoBehaviour
     [SerializeField]
     TransitionMode transitionMode;
 
+    [SerializeField]
+    ComputeShader computeShader;
+
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
+
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+    }
+
     float duration;
 
     bool transitioning;
@@ -54,6 +74,8 @@ public class GPUGraph : MonoBehaviour
             transitionFunction = function;
             PickNextFunction();
         }
+
+        UpdateFunctionOnGPU();
     }
 
     void PickNextFunction()
