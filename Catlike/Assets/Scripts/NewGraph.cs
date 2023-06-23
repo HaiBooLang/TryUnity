b49f84513025ac10a2490619ea0bc09f82ce2034
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 13:13:00 +0800

    Creating a Grid of Points

--- Catlike/Assets/Scripts/NewGraph.cs
+++ Catlike/Assets/Scripts/NewGraph.cs
@@ -21,11 +21,17 @@ public class NewGraph : MonoBehaviour
         Vector3 position = Vector3.zero;
         var scale = Vector3.one * step;
 
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
