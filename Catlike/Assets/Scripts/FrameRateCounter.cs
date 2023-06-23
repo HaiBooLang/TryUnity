b6054f19dd4bccfe5928b343370883ffa6387072
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 16:43:17 +0800

    Average Frame Rate

--- Catlike/Assets/Scripts/FrameRateCounter.cs
+++ Catlike/Assets/Scripts/FrameRateCounter.cs
@@ -1,14 +1,30 @@
 using TMPro;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class FrameRateCounter : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI display;
 
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+
+    int frames;
+
+    float duration;
+
     void Update()
     {
         float frameDuration = Time.unscaledDeltaTime;
-        display.SetText("FPS\n{0:0}\n000\n000", 1f / frameDuration);
+        frames += 1;
+        duration += frameDuration;
+
+        if (duration >= sampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n000\n000", frames / duration);
+            frames = 0;
+            duration = 0f;
+        }
     }
 }
