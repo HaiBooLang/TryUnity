fd49886714a929bf3cdaaa7d83f54921c6696e48
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-23 15:01:00 +0800

    Perturbing the Sphere (1)

--- Catlike/Assets/Scripts/FunctionLibrary.cs
+++ Catlike/Assets/Scripts/FunctionLibrary.cs
@@ -50,11 +50,12 @@ public static class FunctionLibrary
 
     public static Vector3 Sphere(float u, float v, float t)
     {
-        float r = Cos(0.5f * PI * v);
+        float r = 0.9f + 0.1f * Sin(4f * PI * u * t);
+        float s = r * Cos(0.5f * PI * v);
         Vector3 p;
-        p.x = r * Sin(PI * u);
-        p.y = Sin(PI * 0.5f * v);
-        p.z = r * Cos(PI * u);
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(0.5f * PI * v);
+        p.z = s * Cos(PI * u);
         return p;
     }
 }
