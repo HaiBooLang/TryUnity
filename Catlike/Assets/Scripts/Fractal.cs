1a19f2f2e70744580c59c5019b8e4cfaf2e10c09
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-25 14:06:44 +0800

    Cleaning Up

--- Catlike/Assets/Scripts/Fractal.cs
+++ Catlike/Assets/Scripts/Fractal.cs
@@ -6,39 +6,19 @@ public class Fractal : MonoBehaviour
     [SerializeField, Range(1, 8)]
     int depth = 4;
 
-    void Start()
-    {
-        name = "Fractal " + depth;
-        if (depth <= 1)
-        {
-            return;
-        }
+    [SerializeField]
+    Mesh mesh;
 
-        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
-        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
-        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
-        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
+    [SerializeField]
+    Material material;
 
-        childA.transform.SetParent(transform, false);
-        childB.transform.SetParent(transform, false);
-        childC.transform.SetParent(transform, false);
-        childD.transform.SetParent(transform, false);
-        childE.transform.SetParent(transform, false);
-    }
+    static Vector3[] directions = {
+        Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
+    };
 
-    void Update()
-    {
-        transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
-    }
-
-    Fractal CreateChild(Vector3 direction, Quaternion rotation)
-    {
-        Fractal child = Instantiate(this);
-        child.depth = depth - 1;
-        child.transform.localPosition = 0.75f * direction;
-        child.transform.localRotation = rotation;
-        child.transform.localScale = 0.5f * Vector3.one;
-        return child;
-    }
+    static Quaternion[] rotations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
+    };
 }
